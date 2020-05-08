609250f799c79c79d1baeab1e875f3c86984428c
l0stpenguin <>
2020-05-08 16:06:42 +0400

    resize input image

--- DepthData_mob.py
+++ DepthData_mob.py
@@ -102,6 +102,7 @@ class ToTensor(object):
         return {'image': image, 'depth': depth}
 
     def to_tensor(self, pic):
+        pic = pic.resize((640, 480))
         pic = np.array(pic)
         if not (_is_numpy_image(pic) or _is_pil_image(pic)):
                 raise TypeError(  'pic should be PIL Image or ndarray. Got {}'.format(type(pic)))
